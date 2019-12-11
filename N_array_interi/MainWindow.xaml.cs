93088e8b90fef5fa8a2004fa46085a7a381eaec4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-11 13:19:34 +0100

    Add project files.

---
+++ N_array_interi/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace N_array_interi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+
+        private void Genera_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int valore = int.Parse(txtGenera.Text);
+                if (valore <= 0)
+                {
+                    throw new Exception("Il numero deve essere superiore a 0");
+                }
+                int[] array = new int[valore];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(0,20);
+                }
+                lblArray.Content = "[";
+                for (int i = 0; i< array.Length; i++)
+                {
+                    lblArray.Content = lblArray.Content + $"{array[i]}";
+                    if (i < array.Length - 1)
+                        lblArray.Content += ",";
+                }
+                lblArray.Content += "]";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                txtGenera.Text = "";
+            }
+        }
+    }
+}
+
+
+
